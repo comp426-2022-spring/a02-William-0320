00f21b38b29c7a69513b981a0f5d8b5275318c8c
William-0320 <lisiyuan010320@hotmail.com>
2022-04-16 19:33:42 -0400

    Modified flips and guess-flip

--- flips.js
+++ flips.js
@@ -3,4 +3,6 @@ import minimist from "minimist";
 const args = minimist(process.argv.slice(2));
 args["number"];
 const number = args.number || 1;
-console.log(coinFlips(number));
+const flips = coinFlips(number)
+console.log(flips);
+console.log(countFlips(flips))
