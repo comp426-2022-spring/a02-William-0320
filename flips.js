a43f4b4c75f70aecd85c2f5d7358bb24f6c01a5c
William-0320 <lisiyuan010320@hotmail.com>
2022-04-16 20:15:08 -0400

    Corrected variable scope

--- flips.js
+++ flips.js
@@ -2,11 +2,12 @@ import { coinFlip, coinFlips, countFlips, flipACoin } from "./modules/coin.mjs";
 import minimist from "minimist";
 const args = minimist(process.argv.slice(2));
 args["number"];
+const flips = [];
 if(args.number == undefined){
-    const flips = coinFlips(1);
+    flips = coinFlips(1);
 }else{
     const number = args.number;
-    const flips = coinFlips(number);
+    flips = coinFlips(number);
 }
 console.log(flips);
 console.log(countFlips(flips))
