0438b6cfbbf843db678b90c901d9a64a06263562
William-0320 <lisiyuan010320@hotmail.com>
2022-04-16 20:11:35 -0400

    Modified flips with no argument

--- flips.js
+++ flips.js
@@ -2,7 +2,11 @@ import { coinFlip, coinFlips, countFlips, flipACoin } from "./modules/coin.mjs";
 import minimist from "minimist";
 const args = minimist(process.argv.slice(2));
 args["number"];
-const number = args.number || 1;
-const flips = coinFlips(number)
+if(args.number == undefined){
+    const flips = coinFlips(1);
+}else{
+    const number = args.number;
+    const flips = coinFlips(number);
+}
 console.log(flips);
 console.log(countFlips(flips))
